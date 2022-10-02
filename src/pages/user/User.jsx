6dc90e64ff606b93345034f2f30b22a3f32db817
“Akriti <akritiyadav895@gmail.com>
2022-10-02 16:51:35 +0530

    changes done

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./User.css";
+import {LocationSearching, MailOutline, PermIdentity,PhoneAndroid,CalendarToday} from "@material-ui/icons";
 
 export default function User() {
   return (
@@ -13,11 +14,34 @@ export default function User() {
             <div className="userShowTop">
                 <img src="./image/atul.jpg" alt="" className="userShowImg" />
                 <div className="userShowToptitle">
-                    <div className="spanuserShowUsername">Atul yadav</div>
-                    <div className="spanuserShowUserTitle">Software Engineer</div>
+                    <span className="userShowUsername">Atul yadav</span>
+                    <span className="userShowUserTitle">Software Engineer</span>
                 </div>
             </div>
-            <div className="userShowBottom"></div>
+            <div className="userShowBottom">
+              <span className="userShowTitle">Account Details</span>
+              <div className="userShowInfo">
+              <PermIdentity className='userShowIcon'/>
+              <span className="userShowInfoTitle">atuly17</span>
+              </div>
+              <div className="userShowInfo">
+              <CalendarToday className='userShowIcon'/>
+              <span className="userShowInfoTitle">10-12-1998</span>
+              </div>
+              <span className="userShowTitle">Contact Details</span>
+              <div className="userShowInfo">
+              <PhoneAndroid className='userShowIcon'/>
+              <span className="userShowInfoTitle">+1 123 456 68</span>
+              </div>
+              <div className="userShowInfo">
+              <MailOutline className='userShowIcon'/>
+              <span className="userShowInfoTitle">atuly17@gmail.com</span>
+              </div>
+              <div className="userShowInfo">
+              <LocationSearching className='userShowIcon'/>
+              <span className="userShowInfoTitle">Delhi | India</span>
+              </div>
+            </div>
         </div>
         <div className="userUpdate"></div>
      </div>
